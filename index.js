b74fc149e51210bf6d3295850a67bd4e3a3db91f
core <seorisachs@gmail.com>
2018-11-08 19:14:17 -0500

    without puppeteer and txt file

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
-const rp = require('request-promise');
+const request = require('request');
 const cheerio = require('cheerio');
-const puppeteer = require('puppeteer');
-const url = 'https://www.reddit.com/r/ambien/';
+const fs = require('fs');
+const writeStream = fs.createWriteStream('post.txt');
+//const puppeteer = require('puppeteer');
+//const url = 'https://www.reddit.com/';
+//const urls = ['https://www.reddit.com/', 'https://www.reddit.com/r/Maniac/']
 
 
-// try {
-//   rp(url)
-//   function(html) {
-//     console.log(html)
-//   }
 
-// } catch (err) {
-//   console.log(err)
-// }
+request('https://www.reddit.com/','https://www.reddit.com/r/Maniac/', (error,
+response, html) => {
+  if (!error && response.statusCode == 200){
+    const $ = cheerio.load(html);
+
+    $('h2').each((index, element) => {
+      const titles = $(element).text()
+      console.log(titles)
+      writeStream.write(`${titles} \n`)
+    })
+  }
+})
+// request(urls[1], (err, res, body) => {
+//   //Load HTML body into cheerio
+//   const $ = cheerio.load(body);
+//   console.log( $('h2').html())
+//   //Cheerio functions
+// })
+
+//list of promises to the array of urls 
+// let promises = urls.map(url => rp(url))
+
+// Promise.all(promises)
+// .then(([...resolvedUrls])) => {
+//     //resolvedUrls.forEach(url => url.html())
+// })
+
+// puppeteer
+//   .launch()
+//   .then(function(browser) {
+//     return browser.newPage()
+//   })
+//   .then(function(page){
+//     for (let i = 0; i < urls.length; i++){
+//       const url = urls[i]
+//       //console.log(urls.length)
+//       //console.log("PAGE", page)
+//       return page.goto(`${url}`)
+//     }
+//   })
+//   .then(function(html){
+//     cheerio('h2', html).each(function(){
+//       console.log(cheerio(this).text())
+//       writeStream.write(`${cheerio(this).text()} \n`)
+//     })
+//   })
+//   .catch(function(err){
+//     console.log(err)
+//   })
+
 
 //first successful try 
 // rp(url)
@@ -27,22 +72,4 @@ const url = 'https://www.reddit.com/r/ambien/';
 //         console.log(err)
 //     });
 
-puppeteer
-  .launch()
-  .then(function(browser) {
-    return browser.newPage()
-  })
-  .then(function(page){
-    return page.goto(url).then(function(){
-      return page.content();
-    })
-  })
-  .then(function(html){
-    cheerio('h2', html).each(function(){
-      console.log(cheerio(this).text())
-    })
-  })
-  .catch(function(err){
-    console.log(err)
-  })
 
